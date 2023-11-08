49d5637da7454d051a57718a5f3c271eeffb0d44
ricardo4457 <ricardo.a.vieira123@gmail.com>
2023-11-08 15:48:38 +0000

    Teleport

--- src/views/ModalsView.vue
+++ src/views/ModalsView.vue
@@ -3,14 +3,15 @@
         <div class="modals">
             <p>Modal</p>
             <button @click="showModal = true">Show Modal</button>
-
-            <div v-if="showModal">
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique, non, in consequatur consectetur
-                    error
-                    vero itaque, optio nam praesentium veritatis aliquam cumque earum culpa quisquam illo ipsam labore
-                    voluptatibus? Aspernatur.</p>
-                <button @click="showModal = false">Hide Modal</button>
-            </div>
+            <Teleport to="body"> <!-- this tag serves to move a element on html to diferent location -->
+                <div v-if="showModal">
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique, non, in consequatur consectetur
+                        error
+                        vero itaque, optio nam praesentium veritatis aliquam cumque earum culpa quisquam illo ipsam labore
+                        voluptatibus? Aspernatur.</p>
+                    <button @click="showModal = false">Hide Modal</button>
+                </div>
+            </Teleport>
         </div>
     </main>
 </template>
