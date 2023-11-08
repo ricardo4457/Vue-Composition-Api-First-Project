efd44e1ad9b4292f9a353d25d8b0f7656ece2bb6
ricardo4457 <ricardo.a.vieira123@gmail.com>
2023-11-08 15:59:13 +0000

    Creating and importing the child component

---
+++ src/components/Modal.vue
@@ -0,0 +1,25 @@
+<template>
+    <Teleport to="body"> <!-- this tag serves to move a element on html to diferent location -->
+        <div>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique, non, in consequatur consectetur
+                error
+                vero itaque, optio nam praesentium veritatis aliquam cumque earum culpa quisquam illo ipsam labore
+                voluptatibus? Aspernatur.</p>
+            <button>Hide Modal</button>
+        </div>
+    </Teleport>
+</template>
+<script lang="ts" setup>
+//Imports
+import { ref } from 'vue'
+
+</script>
+<style scoped>
+@media (min-width: 1024px) {
+    .modals {
+        min-height: 100vh;
+        display: flex;
+        align-items: center;
+    }
+}
+</style>
--- src/views/ModalsView.vue
+++ src/views/ModalsView.vue
@@ -3,21 +3,14 @@
         <div class="modals">
             <p>Modal</p>
             <button @click="showModal = true">Show Modal</button>
-            <Teleport to="body"> <!-- this tag serves to move a element on html to diferent location -->
-                <div v-if="showModal">
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique, non, in consequatur consectetur
-                        error
-                        vero itaque, optio nam praesentium veritatis aliquam cumque earum culpa quisquam illo ipsam labore
-                        voluptatibus? Aspernatur.</p>
-                    <button @click="showModal = false">Hide Modal</button>
-                </div>
-            </Teleport>
+
         </div>
     </main>
 </template>
 <script lang="ts" setup>
 //Imports
 import { ref } from "vue"
+import Modal from '@/components/Modal.vue'
 
 //Variables
 const showModal = ref(false)
