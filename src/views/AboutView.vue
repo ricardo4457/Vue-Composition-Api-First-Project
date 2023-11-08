2f7035959dd8e3dafd264977c7c2d0ef148a699a
ricardo4457 <ricardo.a.vieira123@gmail.com>
2023-11-08 09:45:35 +0000

    start commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,17 @@
+<template>
+  <main>
+    <div class="about">
+      <p>About</p>
+    </div>
+  </main>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="home">
+    <div class="container">
+      <button @click="decreaseCounter" class="btn">-</button>
+      <span class="counter">{{ counter }}</span>
+      <button @click="increaseCounter" class="btn">+</button>
+    </div>
+  </div>
+</template>
+<script setup>
+import { ref } from 'vue'
+
+const counter = ref(0)
+const increaseCounter = () => {
+  counter.value++
+}
+
+const decreaseCounter = () => {
+  if (counter.value > 0) {
+    counter.value--
+  } else {
+    counter.value = 0
+  }
+}
+</script>
+<style >
+@media (min-width: 1024px) {
+  .home {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+
+.container {
+  align-items: center;
+  padding: 20px;
+}
+
+.btn .counter {
+  font-size: 40px;
+  margin: 10px;
+}
+</style>
