f09485c72aaad405ea4c9de6259f9635af8ea221
ricardo4457 <ricardo.a.vieira123@gmail.com>
2023-11-08 14:47:27 +0000

    useRouter to redirect to spefic page or item

--- src/views/PostsDetailView.vue
+++ src/views/PostsDetailView.vue
@@ -7,20 +7,40 @@
       </p>
     </div>
     <div><button @click="showPostId">Show</button></div>
+    <div><button @click="GoHome">Go Home</button></div>
+    <div><button @click="GoPost1">Go to Post 1</button></div>
   </main>
 </template>I
 <script lang="ts" setup>
 //Imports
-import { useRoute } from 'vue-router';
+import { useRoute, useRouter } from 'vue-router';
 //Variables
 
-const route = useRoute();
+const route = useRoute(); // to get the route parameters
+
+const router = useRouter(); // to redirect to diferent routes on the script
 
 //Methods
 
 const showPostId = () => {
 
   alert("Is Post " + route.params.id);
+
+}
+
+const GoHome = () => {  // In this case goes to home page
+
+  setTimeout(() => {
+    router.push({ name: "home" })
+  }, 3000);
+
+}
+
+const GoPost1 = () => {  // In this case goes to home page
+
+  setTimeout(() => {
+    router.push({ name: "postDetail", params: { id: 'id1' } }) // Goes to a diferent post based on is parameter
+  }, 3000);
 
 }
 
